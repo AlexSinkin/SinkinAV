fb095db1417cb7a6f3c77f88955d9571aac5eb28
AlexSinkin <43829285+AlexSinkin@users.noreply.github.com>
2018-10-24 23:31:52 +0300

    Add files via upload

--- Snowflake_рекурсия.py
+++ Snowflake_рекурсия.py
@@ -1,34 +1,39 @@
 import turtle
 
-def move_turtle (cmd):
-    if cmd=='F':
+
+def move_turtle(cmd):
+    if cmd == 'F':
         turtle.forward(a)
-    elif cmd=='L':
+    elif cmd == 'L':
         turtle.left(60)
     else:
         turtle.right(120)
     return
 
-def kokh_star( moves_in, n ):
+
+def kokh_star(moves_in, n):
     if n == 0:
         return moves_in
     else:
         moves_out = []
         for move in moves_in:
             if move == 'F':
-                moves_out.extend(kokh_star(('F','L','F','R','F','L','F'), n - 1))
+                moves_out.extend(kokh_star(('F', 'L', 'F', 'R', 'F', 'L', 'F'), n - 1))
             else:
+
                 moves_out.append(move)
     return moves_out
 
-# начальные значения
-n = 2
-a = int(500 / ( 3**n ))
 
-turtle.Screen().setup(1000,800, 0, 0)
+# начальные значения
+
+n = 2
+a = int(500 / (3 ** n))
+
+turtle.Screen().setup(1000, 800, 0, 0)
 
 turtle.penup()
-turtle.goto(-200,200)
+turtle.goto(-200, 200)
 turtle.pendown()
 
 # Launch turtle
--- Snowflake_цикл.py
+++ Snowflake_цикл.py
@@ -2,34 +2,35 @@ import turtle
 import graphics as gr
 
 
-def move_turtle (cmd):
-    if cmd=='F':
+def move_turtle(cmd):
+    if cmd == 'F':
         turtle.forward(a)
-    elif cmd=='L':
+    elif cmd == 'L':
         turtle.left(60)
     else:
         turtle.right(120)
     return
 
+
 window = gr.GraphWin("Russian game", 1000, 500)
 
 # начальные значения
 n = 5
-a = int(500/(3**n))
+a = int(500 / (3 ** n))
 
 turtle.penup()
 turtle.goto(-200, 200)
 turtle.pendown()
 
 # программа правильного треугольника
-moves=['F', 'R', 'F', 'R', 'F']
+moves = ['F', 'R', 'F', 'R', 'F']
 
 # цикл из n проходов
 for i in range(n):
-# замена всех F на комбинацию 'FLFRFLF'
+    # замена всех F на комбинацию 'FLFRFLF'
     moves2 = []
     for move in moves:
-        if move=='F':
+        if move == 'F':
             moves2.extend(('F', 'L', 'F', 'R', 'F', 'L', 'F'))
         else:
             moves2.append(move)
@@ -38,7 +39,6 @@ for i in range(n):
 # Launch turtle
 for move in moves:
     move_turtle(move)
-
 
 window.getMouse()
 window.close()
